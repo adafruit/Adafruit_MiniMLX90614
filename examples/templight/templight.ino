8e76efa5894f85482c97985d4f9de63465eca6c8
ladyada <limor@ladyada.net>
2014-03-17 13:33:43 -0400

    GO AMERICA! KILL CELSIUS!

--- examples/templight/templight.ino
+++ examples/templight/templight.ino
@@ -20,29 +20,13 @@ NOT FOR REGULAR ARDUINOS! Use the regular Adafruit_MLX90614 for that
 
   Written by Limor Fried/Ladyada for Adafruit in any redistribution
  ****************************************************/
-/*************************************************** 
-  This is a library example for the MLX90614 Temp Sensor
-
-  Designed specifically to work with the MLX90614 sensors in the
-  adafruit shop
-  ----> https://www.adafruit.com/products/1748
-  ----> https://www.adafruit.com/products/1749
-
-  These sensors use I2C to communicate, 2 pins are required to  
-  interface
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
 
 #include <TinyWireM.h>
 #include <Adafruit_MiniMLX90614.h>
 #include <Adafruit_NeoPixel.h>
 
-// change these to adjust the range of temperatures you want to measure
+// change these to adjust the range of temperatures you want to measure 
+// (these are in Farenheit)
 #define COLDTEMP 60
 #define HOTTEMP  80
 
@@ -60,7 +44,7 @@ void setup() {
 
 void loop() {
   uint8_t red, blue;
-  float temp = mlx.readObjectTempC();
+  float temp = mlx.readObjectTempF();
 
   // map temperature to red/blue color
   // hotter temp -> more red
